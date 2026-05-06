1b089835860725dffed1eaed4ef5fe4d1ee0511c
Eddy Meals <edimea211@gmail.com>
2026-05-06 15:26:06 -0700

    Increase speed for testing program -- it works!

--- controller/main.cpp
+++ controller/main.cpp
@@ -6,17 +6,17 @@
 int main()
 {
   stdio_init_all();
-  while (getchar_timeout_us(2 * 1000 * 1000) == PICO_ERROR_TIMEOUT)
-  {
-    printf("Press any key to begin\n");
-  }
+  //while (getchar_timeout_us(2 * 1000 * 1000) == PICO_ERROR_TIMEOUT)
+  //{
+  //  printf("Press any key to begin\n");
+  //}
   printf("Start\n");
   Motor motor(14, 15);
   printf("Setup done\n");
   while (true)
   {
     printf("Begin move\n");
-    motor.move(400, 200);
+    motor.move(10000, 10000);
     printf("Begin wait\n");
     motor.wait();
   }
